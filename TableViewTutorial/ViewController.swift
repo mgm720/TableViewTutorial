c8e233632cfc29edae3d02f985cb73dd1b019fd2
Michael Miles <michael.gage.miles@gmail.com>
2018-12-31 16:57:35 -0800

    Added data to TableView

--- TableViewTutorial/ViewController.swift
+++ TableViewTutorial/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var array = ["I can create a tableView","Wow I'm doing it","I dont need this guy's help anymore","StepByStep is actually awesome my BAD"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = array[indexPath.row]
+        
+        return cell
+    }
 }
 
